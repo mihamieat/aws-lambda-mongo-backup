811a75215b2207bdf6be137a63c49b6198a56d67
Mihamina Rakotovazaha <r.miham@yahoo.com>
2024-12-16 17:52:30 +0100

    feat: add aws lambda terraform support

--- main.tf
+++ main.tf
@@ -20,3 +20,64 @@ resource "aws_s3_bucket" "mn_mongo_backup" {
     Environment = "Production"
   }
 }
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "MN_DB_backup_Lambda_Function_Role"
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/package/python"
+  output_path = "${path.module}/db-backup.zip"
+}
+
+resource "aws_lambda_function" "terraform_lambda_func" {
+  filename      = "${path.module}/db-backup.zip"
+  function_name = "db_backup"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.10"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
