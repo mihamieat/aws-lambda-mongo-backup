0b3b0327acc6dfd4b6ae7e84184d23db7fb0b4ab
Mihamina Rakotovazaha <r.miham@yahoo.com>
2024-12-16 18:20:19 +0100

    chore: update lambda deploy

--- main.tf
+++ main.tf
@@ -41,7 +41,6 @@ EOF
 }
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
-
   name        = "aws_iam_policy_for_terraform_aws_lambda_role"
   path        = "/"
   description = "AWS IAM Policy for managing AWS Lambda role"
@@ -75,11 +74,25 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_lambda_role" {
   policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
 }
 
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/package/python"
+  output_path = "${path.module}/db-backup.zip"
+}
+
 resource "aws_lambda_function" "terraform_lambda_func" {
   filename      = "${path.module}/db-backup.zip"
   function_name = "db_backup"
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.10"
-  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_lambda_role]
+  environment {
+    variables = {
+      S3_REGION         = "eu-west-3"
+      S3_BUCKET         = "mn-mongo-backup"
+      CONNECTION_STRING = "mongodb+srv://autom_arch:wP8RgsebYtGnYXC3@mn0.4x7e4.mongodb.net/"
+    }
+  }
+  timeout = 30
 }
